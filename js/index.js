a1e6833b9a4a624a3fcd40e5c99bbd849c63bc57
Luan Lopes <58179485+ToxicOtter@users.noreply.github.com>
2022-05-09 21:08:09 -0300

    Aula 09/05
    Mudamos a barra lateral, agora ela segue conforme o usuário desce a página. Tentamos fazer os elementos da barra lateral acompanhar o scroll do usuário, conseguimos parcialmente.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//about us constants
+
 const isaac = document.getElementById("Isaac");
 const pedro = document.getElementById("Pedro");
 const luan = document.getElementById("Luan");
@@ -7,6 +9,21 @@ const isaacLista = document.getElementById("isaacLista");
 const pedroLista = document.getElementById("pedroLista");
 const luanLista = document.getElementById("luanLista");
 const luisLista = document.getElementById("luisLista");
+
+//navbar constant
+
+const navbar = document.getElementById("navbar");
+
+//content constants
+const one = document.getElementById("cont1");
+const two = document.getElementById("cont2");
+const three = document.getElementById("cont3");
+const four = document.getElementById("cont4");
+
+const navList1 = document.getElementById("navList1");
+
+//function to swwitch between the members 
+//add Natan e Matheus 
 
 function sumiu(integrante){
     if(integrante == "Isaac"){
@@ -52,6 +69,36 @@ function sumiu(integrante){
     }
 }
 
+//function to the back-to-top button
+
 function toTheTop(){
     document.documentElement.scrollTop = 0;
 }
+
+
+let scrolled = false;
+window.addEventListener('scroll', (e)=>{
+    if(window.scrollY > 250){
+        navbar.classList.add("follow");
+    }else {
+        navbar.classList.remove("follow");
+    }
+    if(four.getBoundingClientRect().bottom <= window.innerHeight && four.getBoundingClientRect().top >= 0){
+        navList1.style.color = "red"
+    }else {
+        navList1.style.color = "black";
+    }
+    /*if(window.scrollY >= four.getBoundingClientRect().y && window.scrollY <= (four.getBoundingClientRect().y + four.getBoundingClientRect().height)){
+        navList1.style.color = "red";
+        console.log("w " + window.scrollY);
+        console.log("y1 " + four.getBoundingClientRect().y)
+        console.log(four.getBoundingClientRect())
+    }else {
+        navList1.style.color = "black";
+        console.log("w " + window.scrollY);
+        console.log("y1 " + four.getBoundingClientRect().y)
+        console.log(Number(four.getBoundingClientRect().y) + Number(four.getBoundingClientRect().height))
+    }*/
+})
+
+//https://awik.io/check-if-element-is-inside-viewport-with-javascript/
