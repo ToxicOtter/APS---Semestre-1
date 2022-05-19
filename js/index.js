85edd60e4d65ee8678ba6f5fff47beb8f529d9bd
Luan Lopes <58179485+ToxicOtter@users.noreply.github.com>
2022-05-18 22:52:40 -0300

    Several changes
    I reduced the amount of lines on the Javascript file, now the show/hide member function is short and userful. I discarted the function that highlight the navbar items when the content is on the screen, and added a smooth scroll animation. In the end I started the member profile card, I did the struture and the initial style.

--- js/index.js
+++ js/index.js
@@ -1,75 +1,22 @@
-//about us constants
-
-const isaac = document.getElementById("Isaac");
-const pedro = document.getElementById("Pedro");
-const luan = document.getElementById("Luan");
-const luis = document.getElementById("Luis");
-
-const isaacLista = document.getElementById("isaacLista");
-const pedroLista = document.getElementById("pedroLista");
-const luanLista = document.getElementById("luanLista");
-const luisLista = document.getElementById("luisLista");
-
 //navbar constant
-
 const navbar = document.getElementById("navbar");
 
-//content constants
-const contentOne = document.getElementById("cont1");
-const contentTwo = document.getElementById("cont2");
-const contentThree = document.getElementById("cont3");
-const contentFour = document.getElementById("cont4");
+//function to switch between the members content 
+function sumiu(clickedIntegrante){
+    const integrantsList = ['Isaac','Luan','Luis','Matheus','Natan','Pedro'];
+    let index = integrantsList.indexOf(clickedIntegrante);
 
-const navList1 = document.getElementById("navList1");
-const navList2 = document.getElementById("navList2");
-const navList3 = document.getElementById("navList3");
-const navList4 = document.getElementById("navList4");
+    integrantsList.splice(index,1);
 
-//function to swwitch between the members 
-//add Natan e Matheus 
+    document.getElementById(clickedIntegrante).style.visibility = "visible";
+    document.getElementById(clickedIntegrante.toLocaleLowerCase()+"Lista").style.color = "blue";
 
-function sumiu(integrante){
-    if(integrante == "Isaac"){
-        isaac.style.visibility = "visible";
-        pedro.style.visibility = "hidden";
-        luan.style.visibility = "hidden";
-        luis.style.visibility = "hidden";
-
-        isaacLista.style.color = "blue";
-        pedroLista.style.color = "black";
-        luanLista.style.color = "black";
-        luisLista.style.color = "black";
-    } else if(integrante == "Pedro"){
-        isaac.style.visibility = "hidden";
-        pedro.style.visibility = "visible";
-        luan.style.visibility = "hidden";
-        luis.style.visibility = "hidden";
-
-        isaacLista.style.color = "black";
-        pedroLista.style.color = "blue";
-        luanLista.style.color = "black";
-        luisLista.style.color = "black";
-    } else if(integrante == "Luan"){
-        isaac.style.visibility = "hidden";
-        pedro.style.visibility = "hidden";
-        luan.style.visibility = "visible";
-        luis.style.visibility = "hidden";
-
-        isaacLista.style.color = "black";
-        pedroLista.style.color = "black";
-        luanLista.style.color = "blue";
-        luisLista.style.color = "black";
-    } else if(integrante == "Luis"){
-        isaac.style.visibility = "hidden";
-        pedro.style.visibility = "hidden";
-        luan.style.visibility = "hidden";
-        luis.style.visibility = "visible";
-
-        isaacLista.style.color = "black";
-        pedroLista.style.color = "black";
-        luanLista.style.color = "black";
-        luisLista.style.color = "blue";
+    for(let i = 0;i < integrantsList.length;i++){
+        document.getElementById(integrantsList[i]).style.visibility = "hidden";
+        document.getElementById(integrantsList[i].toLocaleLowerCase()+"Lista").style.color = "black";
     }
+
+    integrantsList.push(clickedIntegrante);
 }
 
 //function to the back-to-top button
@@ -78,35 +25,11 @@ function toTheTop(){
     document.documentElement.scrollTop = 0;
 }
 
-
-let scrolled = false;
+//function to fix the navbar when the window is scrolled
 window.addEventListener('scroll', (e)=>{
     if(window.scrollY > 250){
         navbar.classList.add("follow");
     }else {
         navbar.classList.remove("follow");
     }
-
-    if(contentOne.getBoundingClientRect().top <= window.innerHeight && contentOne.getBoundingClientRect().bottom >=0){
-        navList1.style.color = "red"
-    }else {
-        navList1.style.color = "black";
-    }
-    if(contentTwo.getBoundingClientRect().top <= window.innerHeight && contentOne.getBoundingClientRect().bottom >=0){
-        navList2.style.color = "red"
-    }else {
-        navList2.style.color = "black";
-    }
-    if(contentThree.getBoundingClientRect().top <= window.innerHeight && contentOne.getBoundingClientRect().bottom >=0){
-        navList3.style.color = "red"
-    }else {
-        navList3.style.color = "black";
-    }
-    if(contentFour.getBoundingClientRect().top <= window.innerHeight && contentOne.getBoundingClientRect().bottom >=0){
-        navList4.style.color = "red"
-    }else {
-        navList4.style.color = "black";
-    }
 })
-
-//https://awik.io/check-if-element-is-inside-viewport-with-javascript/
