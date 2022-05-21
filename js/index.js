302d8c040ac4628e909c665f3cac1582cbbf6c88
Luan Lopes <58179485+ToxicOtter@users.noreply.github.com>
2022-05-20 23:37:22 -0300

    Member's card update
    The member's card style and logic (appear and disappear) are finished. Some information of the members was added too, but it isn't complete. The function to control data visibility was polished, removing some lines and incresing the funcionality. I started to implement the back-to-the-top fixed button that is showed only when the player scroll down, but only the link is working. I already removed the y scrollbar.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,35 @@
-//navbar constant
+//navbar path
 const navbar = document.getElementById("navbar");
 
+//members informations
+const membersInformations = {
+    Isaac: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Isaac","","#","#"],
+    Luan: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Luan Lima Lopes","","https://github.com/ToxicOtter","https://www.linkedin.com/in/luan-lima-lopes-836672189/"],
+    Luis: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Luis","","#","#"],
+    Matheus: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Matheus","","#","#"],
+    Natan: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Natan","","#","#"],
+    Pedro: ["https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png","Pedro","","#","#"]
+};
+
+//members card paths
+const memberImage = document.getElementById("user-image");
+const memberName = document.getElementById("title");
+const memberDescription = document.getElementById("paragraph");
+const memberGithub = document.getElementById("github");
+const memberLinkedin = document.getElementById("linkedin");
+const card = document.getElementById("userProfile");
+
 //function to switch between the members content 
-function sumiu(clickedIntegrante){
-    const integrantsList = ['Isaac','Luan','Luis','Matheus','Natan','Pedro'];
-    let index = integrantsList.indexOf(clickedIntegrante);
-
-    integrantsList.splice(index,1);
-
-    document.getElementById(clickedIntegrante).style.visibility = "visible";
-    document.getElementById(clickedIntegrante.toLocaleLowerCase()+"Lista").style.color = "blue";
-
-    for(let i = 0;i < integrantsList.length;i++){
-        document.getElementById(integrantsList[i]).style.visibility = "hidden";
-        document.getElementById(integrantsList[i].toLocaleLowerCase()+"Lista").style.color = "black";
+function sumiu(clickedIntegrant){
+    if(window.getComputedStyle(card).visibility == "hidden"){
+        card.style.visibility = "visible"
     }
 
-    integrantsList.push(clickedIntegrante);
+    memberImage.src = membersInformations[clickedIntegrant][0];
+    memberName.innerHTML = membersInformations[clickedIntegrant][1];
+    memberDescription.innerHTML = membersInformations[clickedIntegrant][2];
+    memberGithub.href = membersInformations[clickedIntegrant][3];
+    memberLinkedin.href = membersInformations[clickedIntegrant][4];
 }
 
 //function to the back-to-top button
